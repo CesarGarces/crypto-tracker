133afd791f14d3e9b09fec478f4b4ae3ff3d74cb
cesar garces <cesargarce@gmail.com>
2023-11-04 21:31:54 -0500

    add infinity scroll

--- app/components/CryptoList/CryptoList.tsx
+++ app/components/CryptoList/CryptoList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useId } from 'react';
+import { useEffect, useState, useId, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { type CryptoData, type RootState } from '@/app/types/crypto';
 import { fetchCryptos } from '@/app/reducers/cryptoReducers';
@@ -12,18 +12,33 @@ const CryptoList: React.FC<CryptoData> = () => {
   const dispatch = useDispatch();
 
   const [currencyFilter, setCurrencyFilter] = useState<string>('');
-  const [quantity, setQuantity] = useState<number>(100);
+  const [quantity, setQuantity] = useState<number>(10);
 
-  const longViewId = useId();
   const filterCryptoId = useId();
 
-  useEffect(() => {
-    const fetchAllCryptos = async () => {
-      const allCryptosData = await getAllCryptos(quantity);
-      dispatch(fetchCryptos(allCryptosData));
-    };
-    fetchAllCryptos();
+  const longViewRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    const fetchMoreItems = async () => {
+      const allCryptosData = await getAllCryptos(quantity + 10);
+      dispatch(fetchCryptos(allCryptosData));
+      setQuantity(quantity + 10);
+    }
+    const onIntersection = (entries: IntersectionObserverEntry[]) => {
+      const firstEntry = entries[0];
+      if (firstEntry.isIntersecting && quantity <= 90) {
+        fetchMoreItems();
+      }
+    }
+    const observer = new IntersectionObserver(onIntersection)
+    if (observer && longViewRef.current) {
+      observer.observe(longViewRef.current)
+    }
+    return () => {
+      if (observer) {
+        observer.disconnect()
+      }
+    }
   }, [dispatch, quantity]);
 
   const allCryptos = useSelector((state: RootState) => state.crypto.cryptos);
@@ -41,34 +56,38 @@ const CryptoList: React.FC<CryptoData> = () => {
     setCurrencyFilter(e.target.value);
   }
 
-  const handleLongViewChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setQuantity(Number(e.target.value));
-  }
-
   return (
     <main className="w-full min-h-screen p-20">
       <section>
-        <aside className="flex justify-end">
-          <label htmlFor={longViewId} className="block">
-            <span className="block text-lg font-medium text-slate-50">Show Items</span>
-            <select id={longViewId} onChange={handleLongViewChange} className='text-black p-1 w-full rounded-md'>
-              <option defaultValue={quantity} hidden>{quantity}</option>
-              <option value={10}>10</option>
-              <option value={50}>50</option>
-              <option value={100}>100</option>
-            </select>
-          </label>
-        </aside>
         <label htmlFor={filterCryptoId} className="block">
           <span className="block text-lg font-medium text-slate-50">Search for a cryptocurrency</span>
-          <input
-            id={filterCryptoId}
-            className="w-full rounded-md p-3 text-lg text-black"
-            type="text"
-            placeholder="Type currency name"
-            value={currencyFilter}
-            onChange={handleFilterChange}
-          />
+          <div className="bg-white shadow p-4 rounded-lg dark:bg-gray-800">
+            <div className="flex items-center space-x-2">
+              <svg
+                className=" h-5 w-5 text-gray-500 dark:text-gray-300"
+                fill="none"
+                height="24"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="11" cy="11" r="8" />
+                <path d="m21 21-4.3-4.3" />
+              </svg>
+              <input
+                id={filterCryptoId}
+                className="w-full focus:outline-none dark:bg-gray-700 dark:text-white p-3"
+                placeholder="Type currency name"
+                type="text"
+                value={currencyFilter}
+                onChange={handleFilterChange}
+              />
+            </div>
+          </div>
         </label>
       </section>
       <section className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -83,6 +102,7 @@ const CryptoList: React.FC<CryptoData> = () => {
         }
         {filteredCryptos.length === 0 && <div>No data available</div>}
       </section>
+      {quantity <= 90 && <div ref={longViewRef} className='w-full flex justify-center'>Loadding items...</div>}
     </main>
   );
 };
